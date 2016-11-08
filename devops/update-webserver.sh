c8158a9bfb61ead6ffcad20e4af685a3c539f947
Timo Stankowitz <timo.stankowitz@gmail.com>
2016-11-08 15:16:23 +0100

    update devops script

--- devops/update-webserver.sh
+++ devops/update-webserver.sh
@@ -5,18 +5,17 @@
 # script to autodeploy a docker container after a auto build
 imageName="timodjatomika/sine-website"
 
-
 # check if container is allready running
-docker ps | grep $imageName > /dev/null 2>&1
+docker ps | grep sine-website > /dev/null 2>&1
 checkContainerRunning=$?
 
 if [ "$checkContainerRunning" == "0" ]; then
    # stop and kill container
-   docker kill $(docker ps | grep $imageName)
+   docker kill sine-website
 fi
 
 # pull new image from docker hub
 docker pull $imageName
 
 # and finally start the new docker container
-docker run -d -p 80:80 $imageName
+docker run -d --name sine-website -p 80:80 $imageName
