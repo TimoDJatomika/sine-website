59a657c92a6c075478f1ac3a268d63dcf550639e
Timo Stankowitz <timo.stankowitz@gmail.com>
2016-11-08 14:27:47 +0100

    add devops folder with autodeply script

---
+++ devops/update-webserver.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# author: Timo Stankowitz <timo@brainoftimo.com>
+# create date: 2016-11-08
+
+# script to autodeploy a docker container after a auto build
+imageName="timodjatomika/sine-website"
+
+docker pull $imageName
+
+# check if container is allready running
+docker ps | grep $imageName > /dev/null 2>&1
+checkContainerRunning=$?
+
+if [ "$checkContainerRunning" == "0" ]; then
+   # stop and kill container
+   docker kill $(docker ps | grep $imageName)
+fi
+
+# and finally start the new docker container
+docker run -d -p 80:80 $imageNam
